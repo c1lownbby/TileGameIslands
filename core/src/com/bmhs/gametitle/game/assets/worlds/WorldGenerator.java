7ca01b631951c694f4962db3061224c3da2aaccc
c1lownbby <aliyahsanford123@gmail.com>
2024-01-30 09:49:38 -0800

    island

--- core/src/com/bmhs/gametitle/game/assets/worlds/WorldGenerator.java
+++ core/src/com/bmhs/gametitle/game/assets/worlds/WorldGenerator.java
@@ -15,22 +15,27 @@ public class WorldGenerator {
 
     private int[][] worldIntMap;
 
-    public WorldGenerator (int worldMapRows, int worldMapColumns) {
+    private int seedColor, lightGreen, Green;
+
+    public WorldGenerator(int worldMapRows, int worldMapColumns) {
         this.worldMapRows = worldMapRows;
         this.worldMapColumns = worldMapColumns;
 
         worldIntMap = new int[worldMapRows][worldMapColumns];
 
+        seedColor = 24;
+        lightGreen = 86;
+
 
         Vector2 mapSeed = new Vector2(MathUtils.random(worldIntMap[0].length), MathUtils.random(worldIntMap.length));
         System.out.println(mapSeed.y + " " + mapSeed.x);
 
-        worldIntMap[(int)mapSeed.y][(int)mapSeed.x] = 4;
+        worldIntMap[(int) mapSeed.y][(int) mapSeed.x] = 4;
 
-        for(int r = 0; r < worldIntMap.length; r++){
-            for(int c = 0; c < worldIntMap[r].length; c++) {
-                Vector2 tempVectore = new Vector2(c,r);
-                if(tempVectore.dst(mapSeed) < 10) {
+        for (int r = 0; r < worldIntMap.length; r++) {
+            for (int c = 0; c < worldIntMap[r].length; c++) {
+                Vector2 tempVectore = new Vector2(c, r);
+                if (tempVectore.dst(mapSeed) < 10) {
                     worldIntMap[r][c] = 2;
                 }
 
@@ -40,10 +45,71 @@ public class WorldGenerator {
         //call methods to build 2D array
         //randomize();
         setWater();
-        setSeed();
+        seedIslands(5);
+        searchAndExpand(10, seedColor, lightGreen, 0.99 );
+        searchAndExpand(8, seedColor, 80, 0.85 );
+        searchAndExpand(6, seedColor, 62, 0.55 );
+        searchAndExpand(5, seedColor, 38, 0.65 );
+        searchAndExpand(4, seedColor, 54, 0.25 );
+
+
+
+        //setSeed();
 
         Gdx.app.error("WorldGenerator", "WorldGenerator(WorldTile[][][])");
+
         generateWorldTextFile();
+    }
+
+    private void seedIslands(int num) {
+        for (int i = 0; i < num; i++) {
+            int rSeed = MathUtils.random(worldIntMap.length - 1);
+            int cSeed = MathUtils.random(worldIntMap[0].length - 1);
+            worldIntMap[rSeed][cSeed] = seedColor;
+        }
+    }
+
+    private void searchAndExpand(int radius, int numToFind, int numToWrite, double probability) {
+        for (int r = 0; r < worldIntMap.length; r++) {
+            for (int c = 0; c < worldIntMap[r].length; c++) {
+                if (worldIntMap[r][c] == numToFind) {
+                    for (int subRow = r - radius; subRow <= r + radius; subRow++) {
+                        for (int subCol = c - radius; subCol <= c + radius; subCol++) {
+                            if (subRow >= 0 && subCol >= 0 && subRow <= worldIntMap.length - 1 && subCol <= worldIntMap[0].length - 1 && worldIntMap[subRow][subCol] != numToFind) {
+                                if(Math.random() > probability) {
+                                    worldIntMap[subRow][subCol] = numToWrite;
+
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    private void searchAndExpand(int radius) {
+        for(int r = 0; r < worldIntMap.length; r++){
+            for(int c = 0; c < worldIntMap[r].length; c++){
+
+                if(worldIntMap[r][c] == seedColor) {
+
+                    for(int subRow = r-radius; subRow <= r+radius; subRow++){
+
+                        for(int subCol = c-radius; subCol <= c+radius; subCol++){
+
+                            if(subRow >= 0 && subCol >= 0 && subRow <= worldIntMap.length-1 && subCol <= worldIntMap[0].length-1 && worldIntMap[subRow][subCol] != seedColor) {
+                               worldIntMap[subRow][subCol] = 2;
+                            }
+
+                        }
+                }
+
+            }
+        }
+    }
     }
 
     public String getWorld3DArrayToString() {
@@ -88,6 +154,7 @@ public class WorldGenerator {
             }
         }
     }
+
 
 
     public void setWater() {
